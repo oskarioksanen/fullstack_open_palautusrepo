328f6be67edc0ba03d4824518a321e72c1e5a4b8
Oskari Oksanen <oskari.oksanen@koja.fi>
2025-03-18 17:18:03 +0200

    chore(components): divide App to components

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+  "Refaktoroi sovelluksen koodi siten, että se koostuu kolmesta uudesta "
+  "komponentista: Header, Content ja Total. Kaikki data pidetään edelleen"
+  "komponentissa App, joka välittää tarpeelliset tiedot kullekin "
+  "komponentille props:ien avulla."
 
-function App() {
-  const [count, setCount] = useState(0)
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  "Header huolehtii kurssin nimen renderöimisestä"
+  const Header = (props) => {
+    return (
+      <div>
+        <h1>{props.course}</h1>
+      </div>
+    )
+  }
+
+  "Content osista ja niiden tehtävämääristä"
+  const Content = (props) => {
+    return (
+      <div>
+        <p>{props.parts} {props.exercises}</p>
+      </div>
+    )
+  }
+
+  "Total tehtävien yhteismäärästä."
+  const Total = (props) => {
+    return (
+      <div>
+        <p>Number of exercises {props.exercises}</p>
+      </div>
+    )
+  }
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header course={course}/>
+      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]}/>
+      <Total exercises={[exercises1, exercises2, exercises3]} />
+    </div>
   )
 }
 
